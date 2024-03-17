835502367e3ad7bf83b3532fcc4e414d000229f0
linhnnase170113 <linhnnase170113@fpt.edu.vn>
2024-03-18 00:48:59 +0700

    aaa

--- src/models/ResponseDTO.ts
+++ src/models/ResponseDTO.ts
@@ -3,3 +3,8 @@ export interface ResponseDTO<T> {
     isSuccess: boolean;
     message?: string;
 }
+export interface ResponseDTO<T> {
+    result?: T;
+    isSuccess: boolean;
+    message?: string;
+}
