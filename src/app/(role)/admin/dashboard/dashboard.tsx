3b1d5be7270dc0ff1b560786d4af7bf6345c347b
linhnnase170113 <linhnnase170113@fpt.edu.vn>
2024-03-21 10:08:14 +0700

    logout button dashboard

--- src/app/(role)/admin/dashboard/dashboard.tsx
+++ src/app/(role)/admin/dashboard/dashboard.tsx
@@ -1,10 +1,16 @@
 import Guard from "@/components/Guard";
 import Layout from "@/components/Layout";
+import useAuth from "@/hooks/useAuth";
 
 const Dashboard: React.FC = () => {
+    const { isAuthenticated, logout } = useAuth();
+
     return (
         <Layout>
             <h3>Dashboard</h3>
+            <button style={{ backgroundColor: 'purple', color: 'white',  }} onClick={() => {
+                logout()
+            }}>Log out</button>
         </Layout>
     )
 }
